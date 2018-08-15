3974d05be3d6e6128c01fec803b91b8ed1c2483a
Amy <amyadzuki@gmail.com>
2018-08-15 15:44:36 +0000

    Update noint16p.go

--- flagger/noint16p.go
+++ flagger/noint16p.go
@@ -5,20 +5,20 @@ type NoInt16P struct {
 
 // Int16
 
-func (c *NoInt16P) Int16P(name string, shorthand string, value int16, usage string) *int16 {
+func (fs NoInt16P) Int16P(name string, shorthand string, value int16, usage string) *int16 {
 	panic(NewErrNotSupported())
 }
 
-func (c *NoInt16P) Int16VarP(p *int16, name string, shorthand string, value int16, usage string) {
+func (fs NoInt16P) Int16VarP(p *int16, name string, shorthand string, value int16, usage string) {
 	panic(NewErrNotSupported())
 }
 
 // Uint16
 
-func (c *NoInt16P) Uint16P(name string, shorthand string, value uint16, usage string) *uint16 {
+func (fs NoInt16P) Uint16P(name string, shorthand string, value uint16, usage string) *uint16 {
 	panic(NewErrNotSupported())
 }
 
-func (c *NoInt16P) Uint16VarP(p *uint16, name string, shorthand string, value uint16, usage string) {
+func (fs NoInt16P) Uint16VarP(p *uint16, name string, shorthand string, value uint16, usage string) {
 	panic(NewErrNotSupported())
 }
