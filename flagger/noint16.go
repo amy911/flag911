390bfbd7f19238e17fb3e0a22614a276a20f8d04
Amy <amyadzuki@gmail.com>
2018-08-15 15:41:01 +0000

    Create noint16.go

---
+++ flagger/noint16.go
@@ -0,0 +1,24 @@
+package flagger
+
+type NoInt16 struct {
+}
+
+// Int16
+
+func (c *NoInt16) Int16(name string, value int16, usage string) *int16 {
+	panic(NewErrNotSupported())
+}
+
+func (c *NoInt16) Int16Var(p *int16, name string, value int16, usage string) {
+	panic(NewErrNotSupported())
+}
+
+// Uint16
+
+func (c *NoInt16) Uint16(name string, value uint16, usage string) *uint16 {
+	panic(NewErrNotSupported())
+}
+
+func (c *NoInt16) Uint16Var(p *uint16, name string, value uint16, usage string) {
+	panic(NewErrNotSupported())
+}
