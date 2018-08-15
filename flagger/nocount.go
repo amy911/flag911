bf12af16eda6e0bcff9ae705c01d144ae3e2d12d
Amy <amyadzuki@gmail.com>
2018-08-15 14:49:43 +0000

    Create nocount.go

---
+++ flagger/nocount.go
@@ -0,0 +1,18 @@
+package flagger
+
+type NoCount struct {
+}
+
+func (fs NoCount) CountWorks() bool {
+	return false
+}
+
+// Count
+
+func (fs *NoCount) Count(name string, value int, usage string) *int {
+	panic(NewErrNotSupported())
+}
+
+func (fs *NoCount) CountVar(p *int, name string, value int, usage string) {
+	panic(NewErrNotSupported())
+}
