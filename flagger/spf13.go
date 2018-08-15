167c15e81830dc302dcd3ed7ae11004538024560
Amy <amyadzuki@gmail.com>
2018-08-15 15:51:31 +0000

    Create spf13.go

---
+++ flagger/spf13.go
@@ -0,0 +1,26 @@
+package flagger
+
+import flag "github.com/spf13/pflag"
+
+type SPF13FlagSet struct {
+	NoCount
+	NoCountP
+	NoInt16
+	NoInt16P
+	*flag.FlagSet
+}
+
+// Usage
+
+func (fs SPF13FlagSet) Usage() interface{} {
+	flag.Usage()
+	return nil
+}
+
+func (fs SPF13FlagSet) SetUsage(cb func(interface{}) interface{}, user interface{}, flbk func()) {
+	flag.Usage = flbk
+}
+
+func (fs SPF13FlagSet) PVersionsWork() bool {
+	return true
+}
